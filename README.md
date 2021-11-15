f53bf88ebef3dfad73495d039cfed1d7b814c202
hkim1016 <hankim0827@gmail.com>
2021-11-14 17:56:12 -0800

    Finished part 2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Lab 8 - Starter
 1) I choose option #1, within a Github action that runs whenever code is pushed. This is because we want to make sure that the code that is pushed to the github repository doesn't break the whole project or messes with other parts of the project. We want to continuously make sure that the project is safe, secure, and works whenever something new is added to it.
 2) No
+3) I would not use unit testing to test the "message" of a messaging application. This is because the "message" aspect of the application is the core feature. We need to know how it works with the application as a whole, not just by itself. 
+4) I would use unit testing to test the "max message length" feature. This is because this feature most likely won't interact and break other aspects of the application meaning it is safe to test it by itself. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,82 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test("testing (123)456-7890", () => {
+    expect(functions.isPhoneNumber('(123)456-7890')).toBe(true);
+});
+
+test("testing (858)534-2230", () => {
+    expect(functions.isPhoneNumber('(858)534-2230')).toBe(true);
+});
+
+test("testing 1234567890", () => {
+    expect(functions.isPhoneNumber('1234567890')).toBe(false);
+});
+
+test("testing (123)4567890", () => {
+    expect(functions.isPhoneNumber('(123)4567890')).toBe(false);
+});
+
+test("testing a@b.com", () => {
+    expect(functions.isEmail('a@b.com')).toBe(true);
+});
+
+test("testing admissionsreply@ucsd.edu", () => {
+    expect(functions.isEmail('admissionsreply@ucsd.edu')).toBe(true);
+});
+
+test("testing a@b", () => {
+    expect(functions.isEmail('a@b')).toBe(false);
+});
+
+test("testing a@b.com.com", () => {
+    expect(functions.isEmail('a@b.com.com')).toBe(false);
+});
+
+test("testing 10/16/2001", () => {
+    expect(functions.isDate('10/16/2001')).toBe(true);
+});
+
+test("testing 11/14/2021", () => {
+    expect(functions.isDate('11/14/2021')).toBe(true);
+});
+
+test("testing Oct 16, 2001", () => {
+    expect(functions.isDate('Oct 16, 2001')).toBe(false);
+});
+
+test("testing Nov 14, 2021", () => {
+    expect(functions.isDate('Nov 14, 2021')).toBe(false);
+});
+
+test("testing a123", () => {
+    expect(functions.isStrongPassword('a123')).toBe(true);
+});
+
+test("testing abc1", () => {
+    expect(functions.isStrongPassword('abc1')).toBe(true);
+});
+
+test("testing a", () => {
+    expect(functions.isStrongPassword('a')).toBe(false);
+});
+
+test("testing 1abc", () => {
+    expect(functions.isStrongPassword('1abc')).toBe(false);
+});
+
+test("testing #FFFFFF", () => {
+    expect(functions.isHexColor('#FFFFFF')).toBe(true);
+});
+
+test("testing #123", () => {
+    expect(functions.isHexColor('#123')).toBe(true);
+});
+
+test("testing #FFFF", () => {
+    expect(functions.isHexColor('#FFFF')).toBe(false);
+});
+
+test("testing #1234", () => {
+    expect(functions.isHexColor('#1234')).toBe(false);
+});
